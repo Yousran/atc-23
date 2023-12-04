69329d5b945913d6a7a96d29e2e3b6a4297e6f58
Yousran <79954941+Yousran@users.noreply.github.com>
2023-12-04 22:52:58 +0800

    add yajra datatables library

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -16,19 +16,26 @@ class JobSeeder extends Seeder
     public function run()
     {
         DB::table('jobs')->insert([
-            ['job_name' => 'mahasiswa','add_by' => 'superuser',
+            ['job_name' => 'mahasiswa',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['job_name' => 'pelajar','add_by' => 'superuser',
+            ['job_name' => 'pelajar',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['job_name' => 'wiraswasta','add_by' => 'superuser',
+            ['job_name' => 'wiraswasta',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['job_name' => 'guru','add_by' => 'superuser',
+            ['job_name' => 'guru',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['job_name' => 'ibu rumah tangga','add_by' => 'superuser',
+            ['job_name' => 'ibu rumah tangga',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['job_name' => 'wirausaha','add_by' => 'superuser',
+            ['job_name' => 'wirausaha',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['job_name' => 'tidak sedang bekerja','add_by' => 'superuser',
+            ['job_name' => 'tidak sedang bekerja',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',]
         ]);
     }
--- database/seeders/PaymentStatSeeder.php
+++ database/seeders/PaymentStatSeeder.php
@@ -16,11 +16,14 @@ class PaymentStatSeeder extends Seeder
     public function run()
     {
         DB::table('payment_stats')->insert(
-            ['payment_stat_name' => 'paid','add_by' => 'superuser',
+            ['payment_stat_name' => 'paid',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['payment_stat_name' => 'gradually','add_by' => 'superuser',
+            ['payment_stat_name' => 'gradually'
+            ,'add_by' => 'superuser',
             'updated_by' => 'superuser',],
-            ['payment_stat_name' => 'unpaid','add_by' => 'superuser',
+            ['payment_stat_name' => 'unpaid',
+            'add_by' => 'superuser',
             'updated_by' => 'superuser',],
         );
     }
