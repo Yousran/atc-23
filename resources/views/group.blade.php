1f4067e0b81ebd14d42d7d0c59fa1d8d4e7c91d8
Yousran <79954941+Yousran@users.noreply.github.com>
2023-12-19 23:41:14 +0800

    update view group untuk menampilkan peserta dan jumlah pertemuan

--- resources/views/group.blade.php
+++ resources/views/group.blade.php
@@ -9,6 +9,14 @@
 <x-col ukuran='3'>
     <h2>{{ $group->group_name }}</h2>
     <h3 class="text-secondary">{{ $group->course->course_name }}</h3>
+    <p>Jumlah Pertemuan: {{ $group->num_meet }}</p>
+    <h4>Peserta:</h4>
+    <ul>
+    @foreach ($group->attendants as $attendant)
+        <li>{{ $attendant->data->user->username }}</li>
+    @endforeach
+    </ul>
 </x-col>
-@endforeach 
+@endforeach
+
 @endsection
