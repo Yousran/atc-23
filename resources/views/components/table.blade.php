5181839940583a37ebc8b3d1ad9a8b360950a0d6
Yousran <79954941+Yousran@users.noreply.github.com>
2023-12-20 11:58:15 +0800

    working datatables export

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -16,6 +16,12 @@
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.bootstrap4.min.js"></script>
     <script>
         $(function(){
             $('#{{ $tableName }}').DataTable({
@@ -23,6 +29,11 @@
                 processing: true,
                 serverSide: true,
                 pagingType: 'full_numbers',
+                dom: 'Blfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf'
+                ],
+                lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
                 columns:[
                     @foreach ($columns as $column)
                         @if ($column == 'created_at' || $column == 'updated_at')
@@ -38,4 +49,3 @@
         });
     </script>
 @endpush
-
--- resources/views/tabel.blade.php
+++ resources/views/tabel.blade.php
@@ -3,6 +3,7 @@
 @section('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.bootstrap4.min.css">
 @endsection
 
 @section('columns')    
