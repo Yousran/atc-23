719218dd16101ea1ccc0a0c8555d95476931e6b3
Yousran <79954941+Yousran@users.noreply.github.com>
2023-12-20 13:35:53 +0800

    refactor times table

--- database/seeders/TimeSeeder.php
+++ database/seeders/TimeSeeder.php
@@ -17,19 +17,19 @@ class TimeSeeder extends Seeder
     public function run()
     {
         DB::table('times')->insert([
-            ['time_name' => 'jam pertama','num_time' => '0800',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kedua','num_time' => '0900',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam ketiga','num_time' => '1000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam keempat','num_time' => '1100',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kelima','num_time' => '1200',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam keenam','num_time' => '1300',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam ketujuh','num_time' => '1400',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kedelapan','num_time' => '1500',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kesembilan','num_time' => '1600',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kesepuluh','num_time' => '1700',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kesebelas','num_time' => '1800',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam kedua belas','num_time' => '1900',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['time_name' => 'jam ketiga belas','num_time' => '1200',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam pertama','num_time' => '080000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kedua','num_time' => '090000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam ketiga','num_time' => '100000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam keempat','num_time' => '110000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kelima','num_time' => '120000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam keenam','num_time' => '130000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam ketujuh','num_time' => '140000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kedelapan','num_time' => '150000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kesembilan','num_time' => '160000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kesepuluh','num_time' => '170000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kesebelas','num_time' => '180000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam kedua belas','num_time' => '190000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['time_name' => 'jam ketiga belas','num_time' => '200000',  'add_by' => 'superuser', 'updated_by' => 'superuser', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ]);
     }
 }
